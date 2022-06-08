2832644f8b9730302988030a4e3be0de55940d60
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-06-07 19:03:42 -0500

    added second fetch but doesnt work

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,45 +3,56 @@ var listBoxEl = document.getElementById("cityList");
 var cityInputEl = document.getElementById("citySearch")
 var searchButtonEl = document.getElementById("searchButton")
 var city;
+var latitude;
+var longitude;
 
-var getWeather = function (city) {
+var getLocation = function (city) {
 
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-  // PAID SERVICE?? var weeklyURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&cnt=5&appid=" + APIKey;
 
   fetch(queryURL).then(function (response) {
     //convert to JSON object // 
     if (response.ok) {
       response.json().then(function (data) {
-        console.log(data);
         //extract latitude and longitude to use in a second APIfetch
+        latitude = data.coord.lat;
+        longitude = data.coord.lon;
+        console.log(latitude, longitude);
       })
     } else {
       alert("city not found");
     }
   })
-    //   //Display in HTMl here
-    //   var docArray = data.response.docs;
-    //   for (var i = 0; i < docArray.length; i++) {
-    //     var listItem = document.createElement("li");
-    //     listItem.textContent = docArray[i].description;
-    //     listEl.appendChild(listItem);
-    //   }
-    // })
     .catch(function (error) {
       //in case there is an error
       console.log(error);
     })
 };
 
-var displayWeather = function(conditions) {
-// get conditions temperature, humidity, windspeed, UV index
-// display cityname, date and conditions...with icon
-// color code UV index (favorable/green, moderate/yellow, severe/red)
-// get 5-day forcast
+var getWeather = function () {
+  var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly,alerts&units=imperial&appid=" + APIKey;
+  fetch(weatherURL).then(function (response) {
+    if (response.ok) {
+      console.log(response);
+    } else {
+      alert("invalid city")
+    }
+  })
+  .catch(function(error){
+    console.log(error);
+  })
+}
+
+
+
+var displayWeather = function (conditionsFromWeatherURL) {
+  // get conditions temperature, humidity, windspeed, UV index
+  // display cityname, date and conditions...with icon
+  // color code UV index (favorable/green, moderate/yellow, severe/red)
+  // get 5-day forcast
 };
 
-var addCity = function(cityName){
+var addCity = function (cityName) {
   var newCity = document.createElement("button");
   newCity.textContent = cityName;
   listBoxEl.appendChild(newCity);
@@ -52,9 +63,10 @@ var submitHandler = function (event) {
   event.preventDefault();
   var cityName = cityInputEl.value.trim();
   if (cityName) {
-    getWeather(cityName);
+    getLocation(cityName);
     addCity(cityName);
     cityName = "";
+    getWeather();
   } else {
     alert("Please enter a valid city name");
   };
