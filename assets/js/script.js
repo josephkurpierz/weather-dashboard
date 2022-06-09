85a1cabaf4c4e0286fb09e56b0fdc4ad18005812
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-06-09 11:46:43 -0500

    started rendering current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,28 @@ var APIKey = "d4f5fe65fadf7116d2456da067a274f2";
 var listBoxEl = document.getElementById("cityList");
 var cityInputEl = document.getElementById("citySearch")
 var searchButtonEl = document.getElementById("searchButton")
+var displayBoxEl = document.getElementById("display");
+var forecastBoxEl = document.getElementById("forecast");
 var city;
-var latitude;
-var longitude;
 
 var getLocation = function (city) {
 
-  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+  var queryURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIKey}`;
 
   fetch(queryURL).then(function (response) {
     if (response.ok) {
       //convert to JSON object // 
       response.json().then(function (data) {
-        //extract latitude and longitude to use in a second APIfetch
-        latitude = data.coord.lat;
-        longitude = data.coord.lon;
-        console.log(latitude, longitude);
+        //extract city name, latitude and longitude to use in a second APIfetch
+        var latitude;
+        var longitude;
+        var savedCityName;
+        latitude = data[0].lat;
+        longitude = data[0].lon;
+        savedCityName = data[0].name;
+        var location = { latitude, longitude, savedCityName };
+        getWeather(location);
+        console.log("location", location);
       })
     } else {
       alert("city not found");
@@ -29,41 +35,69 @@ var getLocation = function (city) {
     })
 };
 
-var getWeather = function () {
-  console.log("this runs first even though it is called after the other fetch. I don't understand asynchronous")
-  var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly,alerts&units=imperial&appid=" + APIKey;
+var currentWeather = function (city, current) {
+  //saving pertinent weather parameters to variables
+  var temperature = current.temp;
+  var humidity = current.humidity;
+  var UVIndex = current.uvi;
+  var windspeed = current.wind_speed;
+  var weatherIcon = current.weather[0].icon;
+  
+  console.log("temperature",temperature);
+  // create card to append to html
+  var currentCard = document.createElement("card");
+  currentCard.innerHTML="<h2>"+city+"</h2><p>temperature:"+ temperature+"</p>";
+  //set classes and such
+  displayBoxEl.appendChild(currentCard);
+}
+
+var forecastWeather = function (forecast) {
+  //loop through data"forecast.length
+}
+
+var renderWeather = function (city, data) {
+  currentWeather(city, data.current);
+  forecastWeather(data.daily);
+}
+
+var getWeather = function (location) {
+  // run second API with location from first API to get weather data
+  var weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${location.latitude}&lon=${location.longitude}&exclude=minutely,hourly,alerts&units=imperial&appid=${APIKey}`;
   fetch(weatherURL).then(function (response) {
     if (response.ok) {
-      console.log(response);
-      response.json().then(function(data) {
+      response.json().then(function (data) {
         console.log(data);
+        renderWeather(location.savedCityName, data);
+
+        //render current function
+
         //get current weather data
         // data.current.temp
         // data.current.humidity
         // data.current.uvi
         // data.current.wind_speed
         //data.current.weather[0].icon
-        console.log("temp=",data.current.temp);
-        console.log("uvindex=",data.current.uvi);
-        console.log("icon",data.current.weather[0].icon);
+        console.log("temp=", data.current.temp);
+        console.log("uvindex=", data.current.uvi);
+        console.log("icon", data.current.weather[0].icon);
         // loop through data.daily[] for 5 day from 1-5
         // data.daily[i].humidity
         // data.daily[i].temp.day
         // data.daily[i].uvi
         // data.daily[i].wind_speed
         // data.daily[i].weather[0].icon
-        console.log("tomorrows temp=",data.daily[1].temp.day);
-        console.log("tomorrows windspeed=",data.daily[1].wind_speed);
-        console.log("tomorrows icon=",data.daily[1].weather[0].icon);
+        console.log("tomorrows temp=", data.daily[1].temp.day);
+        console.log("tomorrows windspeed=", data.daily[1].wind_speed);
+        console.log("tomorrows icon=", data.daily[1].weather[0].icon);
 
       })
     } else {
       alert("invalid city")
     }
   })
-  .catch(function(error){
-    console.log(error);
-  })
+    .catch(function (error) {
+      console.log(error);
+    })
 }
 
 
@@ -88,15 +122,15 @@ var submitHandler = function (event) {
   if (cityName) {
     getLocation(cityName);
     addCity(cityName);
-    cityName = "";
-    getWeather();
+    //clear input box...not working
+    cityInputEl.textContent = "";
   } else {
     alert("Please enter a valid city name");
   };
 };
 
 
-//getWeather("forest lake");
+
 
 searchButtonEl.addEventListener("click", submitHandler);
 // listBoxEl.addEventListener("click", getWeather);
